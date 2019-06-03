7f7cd5765518be9d7bff16496814c92e73b20903
wangsenouc <48779115+wangsenouc@users.noreply.github.com>
2019-06-03 22:45:55 +0800

    Add files via upload
    search function

---
+++ index_search.js
@@ -0,0 +1,23 @@
+﻿  commitSearch (res) {
+    let val = ((res.detail || {}).value || '').replace(/\s+/g, '')
+    this.search(val)
+  },
+  clearInput () {
+    this.setData({
+      searchText: '',
+    })
+  },
+  search (val, callback) {
+    wx.pageScrollTo({
+      scrollTop: 0,
+      duration: 300,
+    })
+    if (val) {
+      this.setData({
+        located: false,
+      })
+      this.getWeather(val)
+      //this.getHourly(val)
+    }
+    callback && callback()
+  },
